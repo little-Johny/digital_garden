14982206992620817011e9f351e37ba77ba187d2
little-Johny <molano.johny721@gmail.com>
2026-02-17 19:17:43 -0500

    feat: Implement `compare` function with game logic for guess evaluation.

--- 01-languages/exercism/elixir/guessing-game/lib/guessing_game.ex
+++ 01-languages/exercism/elixir/guessing-game/lib/guessing_game.ex
@@ -1,5 +1,23 @@
 defmodule GuessingGame do
-  def compare(secret_number, guess) do
-    # Please implement the compare/2 function
+
+  def compare(secret_number , guess \\ :no_guess)
+  def compare(_secret_number, :no_guess), do: "Make a guess"
+
+  def compare(s, s) do
+    "Correct"
   end
+
+  def compare(secret_number, guess) when secret_number+1 == guess do
+    "So close"
+  end
+  def compare(secret_number, guess) when secret_number-1 == guess do
+    "So close"
+  end
+
+  def compare(secret_number, guess) when guess > secret_number  do
+    "Too high"
+  end
+  def compare(_secret_number, _guess) , do: "Too low"
+
+
 end
