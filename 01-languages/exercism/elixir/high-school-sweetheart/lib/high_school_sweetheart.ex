dae65d4c94c5011c70961913e7ec8c323cb3599d
little-Johny <molano.johny721@gmail.com>
2026-02-22 16:34:09 -0500

    feat: add initial setup for the High School Sweetheart Elixir exercise.

---
+++ 01-languages/exercism/elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,21 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name) |> String.at(0)
+  end
+
+  def initial(name) do
+    # Please implement the initial/1 function
+  end
+
+  def initials(full_name) do
+    # Please implement the initials/1 function
+  end
+
+  def pair(full_name1, full_name2) do
+    # ❤-------------------❤
+    # |  X. X.  +  X. X.  |
+    # ❤-------------------❤
+
+    # Please implement the pair/2 function
+  end
+end
