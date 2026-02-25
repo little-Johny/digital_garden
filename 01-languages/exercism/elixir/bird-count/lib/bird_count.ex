1d0f6c371b6cbeef39fe014ad042975c436ce2c5
little-Johny <molano.johny721@gmail.com>
2026-02-25 18:15:16 -0500

    feat: Add high-score Elixir Exercism exercise files and update bird-count solution.

--- 01-languages/exercism/elixir/bird-count/lib/bird_count.ex
+++ 01-languages/exercism/elixir/bird-count/lib/bird_count.ex
@@ -1,21 +1,35 @@
 defmodule BirdCount do
-  def today(list) do
-    # Please implement the today/1 function
+  def today(list)
+  def today([]), do: nil
+  def today([head | _tail]), do: head
+
+
+  def increment_day_count(list)
+  def increment_day_count([]), do: [1]
+  def increment_day_count([head | tail]) do
+    [ head + 1 | tail]
   end
 
-  def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+  def has_day_without_birds?(list)
+  def has_day_without_birds?([0 | _tail]) do
+    true
+  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([_head | tail]) do
+    has_day_without_birds?(tail)
   end
 
-  def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+  def total(list)
+  def total([]), do: 0
+  def total([head | tail ]) do
+    head + total(tail)
   end
 
-  def total(list) do
-    # Please implement the total/1 function
+  def busy_days(list)
+  def busy_days([]), do: 0
+  def busy_days([head | tail]) when head >= 5 do
+    1 + busy_days(tail)
   end
+  def busy_days([_head | tail]), do: busy_days(tail)
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
 end
