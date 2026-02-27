bd91901c0e953bec11ff688b56e1a4763839cfa5
little-Johny <molano.johny721@gmail.com>
2026-02-27 10:27:11 -0500

    feat: Resolve high score exercism #12

--- 01-languages/exercism/elixir/high-score/lib/high_score.ex
+++ 01-languages/exercism/elixir/high-score/lib/high_score.ex
@@ -1,25 +1,24 @@
 defmodule HighScore do
-  def new() do
-    # Please implement the new/0 function
-  end
+  @initial_score 0
+  def new(), do: %{}
 
-  def add_player(scores, name, score) do
-    # Please implement the add_player/3 function
+  def add_player(scores, name, score \\ @initial_score) do
+    Map.put(scores, name, score)
   end
 
   def remove_player(scores, name) do
-    # Please implement the remove_player/2 function
+    Map.delete(scores, name)
   end
 
   def reset_score(scores, name) do
-    # Please implement the reset_score/2 function
+    Map.put(scores, name, @initial_score)
   end
 
   def update_score(scores, name, score) do
-    # Please implement the update_score/3 function
+    Map.update(scores, name, score, fn old_score -> old_score + score end)
   end
 
   def get_players(scores) do
-    # Please implement the get_players/1 function
+    Map.keys(scores)
   end
 end
