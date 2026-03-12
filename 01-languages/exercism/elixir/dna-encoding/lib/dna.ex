aa5e6f0f15e75ff3c601263854b7a41c4c4472e7
little-Johny <molano.johny721@gmail.com>
2026-03-11 22:45:59 -0500

    feat: Resolve DNA Encoding exercism #20

--- 01-languages/exercism/elixir/dna-encoding/lib/dna.ex
+++ 01-languages/exercism/elixir/dna-encoding/lib/dna.ex
@@ -1,17 +1,40 @@
 defmodule DNA do
-  def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
-  end
+  def encode_nucleotide(code_point)
+  def encode_nucleotide(?A), do: 0b0001
+  def encode_nucleotide(?C), do: 0b0010
+  def encode_nucleotide(?G), do: 0b0100
+  def encode_nucleotide(?T), do: 0b1000
+  def encode_nucleotide(?\s), do: 0b0000
 
-  def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
-  end
+  def decode_nucleotide(encoded_code)
+  def decode_nucleotide(0b0001), do: ?A
+  def decode_nucleotide(0b0010), do: ?C
+  def decode_nucleotide(0b0100), do: ?G
+  def decode_nucleotide(0b1000), do: ?T
+  def decode_nucleotide(0b0000), do: ?\s
 
   def encode(dna) do
-    # Please implement the encode/1 function
+    do_encode(dna, <<>>)
+  end
+
+  defp do_encode(dna, acc)
+  defp do_encode([], acc), do: acc
+
+  defp do_encode([head | tail], acc) do
+    new = encode_nucleotide(head)
+    acc = <<acc::bitstring, new::4>>
+    do_encode(tail, acc)
   end
 
   def decode(dna) do
-    # Please implement the decode/1 function
+    do_decode(dna,[])
+  end
+
+  defp do_decode(dna, acc)
+  defp do_decode(<<>>, acc), do: acc
+  defp do_decode(<<head::4, tail::bitstring>>, acc) do
+    new = decode_nucleotide(head)
+    acc = acc ++ [new]
+    do_decode(tail, acc)
   end
 end
