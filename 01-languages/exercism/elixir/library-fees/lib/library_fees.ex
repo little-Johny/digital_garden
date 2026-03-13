40fe6baade94577a138527472dd15e1c3fbcc1a1
little-Johny <jmolano@mono.la>
2026-03-13 16:47:09 -0500

    feat: Resolve Library Fees exercism #21

--- 01-languages/exercism/elixir/library-fees/lib/library_fees.ex
+++ 01-languages/exercism/elixir/library-fees/lib/library_fees.ex
@@ -1,25 +1,40 @@
 defmodule LibraryFees do
   def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
+    NaiveDateTime.from_iso8601!(string)
   end
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    datetime.hour < 12
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    date = NaiveDateTime.to_date(checkout_datetime)
+    if before_noon?(checkout_datetime) do
+      Date.add(date, 28)
+    else
+      Date.add(date, 29)
+    end
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    actual_return_datetime |> NaiveDateTime.to_date() |> Date.diff(planned_return_date) |> max(0)
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    datetime |> NaiveDateTime.to_date() |> Date.day_of_week() |> Kernel.==(1)
+
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    checkout_date = datetime_from_string(checkout)
+    real_return_date = datetime_from_string(return)
+    planned_return_date = return_date(checkout_date)
+    real_diff = days_late(planned_return_date, real_return_date)
+
+    if monday?(real_return_date) do
+      div(real_diff * rate, 2)
+    else
+      real_diff * rate
+    end
   end
 end
